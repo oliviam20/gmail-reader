6f3e449c8f674017e6f2ca0bda346e8468f69818
Olivia M <oliviatrademark@gmail.com>
2024-07-28 14:53:14 +1000

    first commit

---
+++ src/GoogleApp.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
+
+function GoogleApp() {
+  const [accessToken, setAccessToken] = useState(null);
+  const [emails, setEmails] = useState([]);
+
+  const login = useGoogleLogin({
+    onSuccess: (response) => {
+      console.log('Login Success:', response);
+      setAccessToken(response.access_token);
+    },
+    onError: (error) => console.log('Login Failed:', error),
+    scope: 'https://www.googleapis.com/auth/gmail.readonly',
+  });
+
+  const fetchEmails = async () => {
+    if (!accessToken) {
+      console.error('Access token is not available.');
+      return;
+    }
+
+    try {
+      const res = await axios.get('https://www.googleapis.com/gmail/v1/users/me/messages', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      console.log('data', res.data)
+
+      if (res.data.messages.length) {
+        const emailContent = await Promise.all(res.data.messages.map(message => axios.get(`https://www.googleapis.com/gmail/v1/users/me/messages/${message.id}`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })))
+        if (emailContent.length) {
+          console.log('emailContent', emailContent)
+          const emailData = emailContent.map(email => email.data)
+          setEmails(emailData);
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching emails:', error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center p-8">
+      <div className="flex flex-col gap-2 max-w-xl">
+        <h2 className="text-xl p-b-3">Google Auth in React</h2>
+        {!accessToken ? (
+          <button onClick={() => login()}>Login with Google</button>
+        ) : (
+          <div>
+            <button className="mb-2" onClick={fetchEmails}>Fetch Emails</button>
+            {emails.map((email) => {
+              const subject = email.payload.headers.find(header => header.name === 'Subject')?.value;
+              const from = email.payload.headers.find(header => header.name === 'From')?.value;
+              return (
+              <div key={email.id} className="border border-sky-500 rounded p-2 mb-4">
+                <div className="pb-2">
+                  <h3 className="text-lg font-bold">Email id</h3>
+                  <p>{email.id}</p>
+                </div>
+                <div className="pb-2">
+                  <h3 className="text-lg font-bold">Labels</h3>
+                  {email.labelIds.map(label => <p key={label}>{label}</p>)}
+                </div>
+                <div className="pb-2">
+                  <h3 className="text-lg font-bold">Sender</h3>
+                  <p>{from}</p>
+                </div>
+                <div className="pb-2">
+                  <h3 className="text-lg font-bold">Subject</h3>
+                  <p>{subject}</p>
+                </div>
+                <div className="pb-2">
+                  <h3 className="text-lg font-bold">Snippet</h3>
+                  <p>{email.snippet}</p>
+                </div>
+              </div>
+            )})}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default GoogleApp;
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import GoogleApp from './GoogleApp.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={process.env.GMAIL_CLIENT_ID!}>
+      <GoogleApp />
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
+)
