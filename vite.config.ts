3426c4b1e1b7233d358d52d1e4d5715022472219
Olivia M <oliviatrademark@gmail.com>
2024-08-27 09:30:37 +1000

    chore: clean up unused code

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd(), '');
-//   return {
-//     base: '/gmail-reader/',
-//     define: {
-//       'process.env.GMAIL_CLIENT_ID': JSON.stringify(env.GMAIL_CLIENT_ID)
-//     },
-//     plugins: [react()],
-//     css: {
-//       postcss: {
-//         plugins: [tailwindcss()],
-//       },
-//     },
-//   }
-// })
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
